12c74ec26742bf900af118df542c385e663860cb
Jeff Chiu <jwchiu82@gmail.com>
2021-01-08 16:29:05 -0800

    add background image and work on displaying splash page with engineer & machine roster

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet("/")]
+        private readonly FactoryContext _db;
+
+        public HomeController(FactoryContext db)
+        {
+            _db = db;
+        }
+        //[HttpGet("/")]
         public ActionResult Index()
         {
-            return View();
+            List<Engineer> model = _db.Engineers.ToList();
+            return View(model);
         }
     }
 }
