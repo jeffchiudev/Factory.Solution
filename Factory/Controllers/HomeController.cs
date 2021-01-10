c242f3b88f638def2cea2cb54a058ba7322dfdeb
Jeff Chiu <jwchiu82@gmail.com>
2021-01-09 21:53:42 -0800

    remove unnecessary using directives

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,9 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using System.Linq;
 using Factory.Models;
 using System.Dynamic;
-using Microsoft.EntityFrameworkCore;
 
 namespace Factory.Controllers
 {
